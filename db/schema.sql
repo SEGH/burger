b2b734907b592cba3007dea6a8665304ae7a3277
Susan Holland <segh@fastmail.com>
2020-07-13 16:55:08 -0400

    Added default false to devoured column and drop database if exists

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS burgers_db;
+
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
@@ -5,5 +7,5 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(30) NOT NULL,
-    devoured BOOLEAN
+    devoured BOOLEAN DEFAULT false
 )
