fc12b808240a783dc39970627e58dbab92a82315
Susan Holland <segh@fastmail.com>
2020-07-13 17:56:54 -0400

    Added handlebars, PORT, app.listen to server

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
+// Dependencies
 const express = require("express");
+const exphbs = require("express-handlebars");
 
+const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+// Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Start Server
+app.listen(PORT, function() {
+    console.log("Server is listening on port " + PORT);
+});
