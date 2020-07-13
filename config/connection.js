5d0ac1c3f27d836424a0d0b0ed54b464db0f7ec2
Susan Holland <segh@fastmail.com>
2020-07-13 17:11:23 -0400

    Wrote code to set up, connect, and export connection

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// Set up to connect Node to MySQL
+const mysql = require("mysql");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "burgers_db"
+});
+
+// Run connection
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection
+module.exports = connection;
