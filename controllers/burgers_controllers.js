dffaaf8adc80c8874cb6480b94d8e88254b96075
Susan Holland <segh@fastmail.com>
2020-07-13 18:05:37 -0400

    Required dependencies, set up router to render index and exported

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,5 +1,13 @@
 // Require express, burger.js
+const express = require("express");
+const burger = require("../models/burger.js");
 
 // Create app router
+const router = express.Router();
+
+router.get("/", function(req, res) {
+    res.render("index");
+});
 
 // Export router
+module.exports = router;
