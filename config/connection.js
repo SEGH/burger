2d5f16c3cc9299fffb9f05b9bb541500a5d0f32a
Susan Holland <segh@fastmail.com>
2020-07-14 19:11:38 -0400

    declared connection with let instead of const

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up to connect Node to MySQL
 const mysql = require("mysql");
 require("dotenv").config();
-const connection;
+let connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
