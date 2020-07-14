c6eb46b9395551db6f3db748847e501ace847374
Susan Holland <segh@fastmail.com>
2020-07-13 21:13:38 -0400

    Imported orm, called orm selectAll function in burger object, exported burger object

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,14 @@
 // Require orm.js
+const orm = require("../config/orm.js");
 
 // Call ORM functions using burger specific input for the ORM
+const burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    }
+}
 
 // Export
+module.exports = burger;
