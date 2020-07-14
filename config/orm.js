9184878b703ba4d51af56980a548c1b8527bff1b
Susan Holland <segh@fastmail.com>
2020-07-13 21:12:24 -0400

    Imported connection, wrote selectAll function, exported orm

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,19 @@
 // Require connection.js
+const connection = require("../config/connection.js");
 
-// selectAll() method to query SQL
+const orm = {
+    // selectAll() method to query SQL
+    selectAll: function(tableInput, callBack) {
+        connection.query("SELECT * FROM ??", [tableInput], function(err, res) {
+            if (err) throw err;
 
+            callBack(res);
+        });
+    }
 // insertOne() method to query SQL
 
 // updateOne() method to query SQL
+}
 
 // Export ORM object
+module.exports = orm;
