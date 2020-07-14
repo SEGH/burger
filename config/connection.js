757557ad2e4899e0b0d42ca20f05ef006b4828ed
Susan Holland <segh@fastmail.com>
2020-07-14 18:20:23 -0400

    Added JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,22 @@
 // Set up to connect Node to MySQL
 const mysql = require("mysql");
 require("dotenv").config();
+const connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+}
 
 // Run connection
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
         return;
